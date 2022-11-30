6fe5a2a1cbc057d1f96f39193f7c2a602a591c11
nozwock <57829219+nozwock@users.noreply.github.com>
2022-11-30 21:22:21 +0530

    prep: add a simple parse func

--- 2022/rust/src/input.rs
+++ 2022/rust/src/input.rs
@@ -1,8 +1,35 @@
-use anyhow::Result;
-use std::io::{self, Read};
+use std::{
+    io::{self, Read},
+    str::FromStr,
+};
 
-pub fn get_stdin_input() -> Result<String> {
+/// Reads stdin and return String
+pub fn get_stdin_input() -> anyhow::Result<String> {
     let mut buf = String::new();
     io::stdin().lock().read_to_string(&mut buf)?;
     Ok(buf)
+}
+
+/// Parse input items seperated by a delimeter
+///
+/// # Examples
+/// ```
+/// use aoc::prelude::*;
+///
+/// fn main() {
+///     let input = get_stdin_input().unwrap();
+///     let newline_sep: Vec<i32> = parse_separated_by(&input, '\n').unwrap();
+///     let csv_type: Vec<i32> = parse_separated_by(&input, ',').unwrap();
+/// }
+/// ```
+pub fn parse_separated_by<T>(raw: &str, delim: char) -> Option<Vec<T>>
+where
+    T: FromStr,
+{
+    raw.trim()
+        .split(delim)
+        .filter(|s| !s.trim().is_empty())
+        .map(|s| s.trim().parse())
+        .collect::<Result<Vec<_>, _>>()
+        .ok()
 }
--- 2022/rust/src/lib.rs
+++ 2022/rust/src/lib.rs
@@ -1,2 +1,10 @@
-mod input;
-pub mod prelude;
+//! Kinda my on own mini advtools
+pub mod input;
+
+pub mod prelude {
+    pub use crate::input::*;
+    pub use itertools::Itertools;
+    pub use std::collections::HashMap;
+}
+
+// Following modules are solution related
