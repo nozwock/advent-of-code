ab0284add43519f76e08dde8f4dd62bd5cd36cae
nozwock <57829219+nozwock@users.noreply.github.com>
2022-12-03 21:19:18 +0530

    2022: add day1 part1 solution

---
+++ 2022/rust/src/bin/day3.rs
@@ -0,0 +1,37 @@
+use aoc::prelude::*;
+
+fn get_item_priority(item: char) -> usize {
+    let mut items = (b'A'..=b'z')
+        .map(|c| c as char)
+        .filter(|c| c.is_alphabetic())
+        .collect_vec();
+    items.rotate_left(26);
+    items
+        .iter()
+        .find_position(|&c| c.eq(&item))
+        .expect("you deserve it")
+        .0
+        + 1
+}
+
+fn main() -> Result<()> {
+    let input = get_stdin_input()?;
+
+    // Part 1
+    println!(
+        "Part 1: {}",
+        input
+            .lines()
+            .map(|racksack| racksack.split_at(racksack.len() / 2))
+            .map(|(compart1, compart2)| compart1.chars().filter(|c| compart2.contains(*c)).dedup())
+            .fold(vec![], |mut accum, wrongs| {
+                accum.extend(wrongs);
+                accum
+            })
+            .into_iter()
+            .map(get_item_priority)
+            .sum::<usize>()
+    );
+
+    Ok(())
+}
